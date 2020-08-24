d7e9720d5bcd6837f8818fea240c20b387075479
Raphael Müller <raphaelplmuller@icloud.com>
2020-08-24 13:08:18 -0300

    Format files

--- server/src/database/connection.js
+++ server/src/database/connection.js
@@ -1,6 +1,8 @@
-const knex = require('knex');
-const config = require('../../../knexfile');
+const knex = require("knex");
+const config = require("../../../knexfile");
 
-const connection = process.env.DEVELOPMENT_DATABASE_NAME ? knex(config.development) : knex(config.production);
+const connection = process.env.DEVELOPMENT_DATABASE_NAME
+  ? knex(config.development)
+  : knex(config.production);
 
 module.exports = connection;
